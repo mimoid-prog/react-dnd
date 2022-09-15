7cd80dcdaf1801c555b3d23189ab9845b67c82a9
Mateusz Szkop <mimoid.prog@gmail.com>
2022-09-15 12:27:13 +0200

    add

---
+++ src/Box.tsx
@@ -0,0 +1,28 @@
+import { useDrag } from "react-dnd";
+import { DnDItem, DND_ITEM_TYPE } from "./data";
+
+interface Props {
+  id: string;
+}
+
+function Box({ id }: Props) {
+  const [{ isDragging }, drag] = useDrag<
+    DnDItem,
+    unknown,
+    { isDragging: boolean }
+  >({
+    type: DND_ITEM_TYPE,
+    item: () => ({
+      id,
+    }),
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <div className={`dragSource ${isDragging ? "dragging" : ""}`} ref={drag} />
+  );
+}
+
+export default Box;
---
+++ src/DropTarget.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { useDrop } from "react-dnd";
+import { DnDItem, DND_ITEM_TYPE } from "./data";
+
+interface Props {
+  children: ReactNode;
+  onDrop: (id: string) => void;
+}
+
+function DropTarget({ children, onDrop }: Props): JSX.Element {
+  const [, drop] = useDrop<DnDItem, void, { isOver: boolean }>({
+    accept: DND_ITEM_TYPE,
+    drop: (item) => {
+      onDrop(item.id);
+    },
+  });
+
+  return (
+    <div className="dropTarget" ref={drop}>
+      {children}
+    </div>
+  );
+}
+
+export default DropTarget;
